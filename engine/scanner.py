3370495eb4a9598c194a96f93264ab8797a68076
Chris Wilson <chris@chrisjwilson.com>
2013-09-09 09:57:35 +1000

    added rstrip to scanner.py

--- engine/scanner.py
+++ engine/scanner.py
@@ -4,16 +4,10 @@ import string
 import datetime
 from cymruwhois import Client
 from async_dns import AsyncResolver
-<<<<<<< HEAD
-=======
-with open('db\active.db') as f:
-    content = f.readlines()
-ar = AsyncResolver([content])
->>>>>>> 0e94139c1735c5c773f96b1d0b6dc95821cce300
 while (True): 
 	with open('db/active.db') as f:
 		content = f.readlines()
-	myList = [i.split('|')[0] for i in content] 
+	myList = [i.split('|')[0].rstrip() for i in content] 
 	ar = AsyncResolver(myList)
 	resolved = ar.resolve()
 	for host, ip in resolved.items():	
